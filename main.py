04baecc6b88ecd20803681f59c3a952bd93be50a
wujitao <376346470@qq.com>
2023-07-18 13:41:21 +0800

    update import path

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 
 from PySide2.QtWidgets import QApplication
-from client import MainWidget
+from src.front.client import MainWidget
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
