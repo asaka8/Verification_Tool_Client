680b3724bc60b09704cba58a596434409664d7f8
wujitao <376346470@qq.com>
2023-07-18 13:56:39 +0800

    optimized code logic

--- src/test_framwork/Test_Cases.py
+++ src/test_framwork/Test_Cases.py
@@ -5,22 +5,19 @@ from .Test_Logger import TestLogger
 from ..conmunicator.print_center import pass_print, error_print
 
 class Test_Section:
-    _section_number = 0
-
     def __init__(self, section_name, section_id):
         self.section_name = section_name
         self.section_id = section_id
         self.test_cases = []
         self.total_test_cases = 0
-        self.mx = None
 
     def add_test_case(self, test_case):
         self.test_cases.append(test_case)
         self.total_test_cases += 1
         test_case.test_id = self.total_test_cases
 
-    def run_test_section(self, case_id=None, text_browser=None):
-        if case_id is None:
+    def run_test_section(self, text_browser=None, debug=False):
+        if debug == False:
             counter = 0
             for test in self.test_cases:
                 # text_browser.append(f'{self.section_id}. {self.section_name}\r\n')
@@ -34,31 +31,29 @@ class Test_Section:
                     id = f'E{self.section_id}-{caseID} '
                 test.run_test_case(id, text_browser=text_browser)
         else:
-            counter = case_id
-            for i in range(len(self.test_cases)):
-                if i == counter:
-                    text_browser.append(f'{self.section_id}. {self.section_name}\r\n')
-                    text_browser.moveCursor(text_browser.textCursor().End)
-                    caseID = str(i + 1)
-                    if len(caseID) == 1:
-                        id = f'E{self.section_id}-00{caseID} '
-                    elif len(caseID) == 2:
-                        id = f'E{self.section_id}-0{caseID} '
-                    else:
-                        id = f'E{self.section_id}-{caseID} '
-                    self.test_cases[i].run_test_case(id ,text_browser=text_browser)
+            for case in self.test_cases:
+                text_browser.append(f'{self.section_id}. {self.section_name}\r\n')
+                text_browser.moveCursor(text_browser.textCursor().End)
+                caseID = str(case.case_id)
+                # print(caseID)
+                if len(caseID) == 1:
+                    id = f'E{self.section_id}-00{caseID} '
+                elif len(caseID) == 2:
+                    id = f'E{self.section_id}-0{caseID} '
+                else:
+                    id = f'E{self.section_id}-{caseID} '
+                case.run_test_case(id ,text_browser=text_browser)
 
 #############################################
 
 class Test_Case:
-
-    def __init__(self, case_name, handle = None, cmd = None, param = None):
+    def __init__(self, case_name, handle=None, cmd=None, param=None, case_id=None):
         self.test_case_name = case_name
         self.handle = handle
         self.result = []
         self.cmd = cmd
         self.param = param
-        self.test_id = 0
+        self.case_id = case_id
 
     def _prepare_result(self, response):
         expected_res = ''
@@ -86,7 +81,7 @@ class Test_Case:
         else:
             expected_res = response[2]
 
-        self.result = { 'id': self.test_id,
+        self.result = { 'id': self.case_id,
                         'test_name': self.test_case_name,
                         'expected': expected_res,
                         'actual': actual_res,
